abc5bfb12677113d533b14e73c42cefc3536bce2
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-10-02 12:23:02 +0900

    ほぞん

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+template <typename Type>
+
+Type Min(Type a, Type b) {
+	return a < b ? a : b;
+}
+
+template<>
+char Min<char>(char a, char b) {
+	return printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ\n");
+}
+
+int main(void) {
+
+	
+
+	return 0;
+}
