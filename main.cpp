e7ad8a43bdf7c349c4315bfc534c9f8e5c719094
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-10-02 14:59:23 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
 ﻿#include <stdio.h>
 
+int Recursive(int n) {
+	if (n <= 100) {
+		return n * 2 - 50;
+	}
+	return (n * Recursive(n - 1));
+}
+
 int main(void) {
+	
+	int n = 1;
+	int select = 0;
+
+	printf("1か2を入力 : ");
+	scanf_s("%d", select);
+
+	if (select == 1) {
+		printf("一般的な資金体系");
+	}
+	else if (select == 2) {
+		if (n == 1) {
+			printf("100円スタート");
+			n++;
+		}
+		else if (n > 1) {
+			printf("%d", Recursive(n));
+			n++;
+		}
+	}
 
 }
