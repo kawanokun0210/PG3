20b66690cade11da1f6ca1b8e044661b18386d70
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-11-06 15:16:01 +0900

    セーブ

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ template <class Type1, class Type2, class Type3 = Type1>
 class Compare {
 public:
 
-	Compare(Type1 a, Type2 b) : a(a),b(b){}
+	Compare(Type1 a, Type2 b) : a(a), b(b) {}
 
 	Type3 Min() {
-		return a < b ? static_cast<Type3>(a) : static_cast<Type3>(b)
+		return a < b ? static_cast<Type3>(a) : static_cast<Type3>(b);
 	}
 
 private:
@@ -18,7 +18,7 @@ private:
 
 int main(void) {
 
-	Compare<int, float> Double(10, 12.0f);
+	Compare<int, float, double> Double(10, 12.0f);
 	printf("%f", Double.Min());
 
 	return 0;
