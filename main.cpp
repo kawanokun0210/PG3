2d07e91cf2374128a5515bd3b627544766e26177
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-11-13 12:51:09 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,25 +1,40 @@
 ﻿#include <stdio.h>
 
-template <class Type1, class Type2, class Type3 = Type1>
+template <typename Type1, typename Type2>
 
-class Compare {
+class Pair
+{
 public:
 
-	Compare(Type1 a, Type2 b) : a(a), b(b) {}
+	Pair(Type1 type1, Type2 type2) : num1(type1), num2(type2) {}
 
-	Type3 Min() {
-		return a < b ? static_cast<Type3>(a) : static_cast<Type3>(b);
+	template<typename Type>
+	Type Min()
+	{
+		Type type1 = static_cast<Type>(num1);
+		Type type2 = static_cast<Type>(num2);
+		return type1 < type2 ? type1 : type2;
 	}
 
-private:
-	Type1 a;
-	Type2 b;
+	Type1 num1;
+	Type2 num2;
 };
 
 int main(void) {
 
-	Compare<int, float, double> Double(10, 12.0f);
-	printf("%f", Double.Min());
+	Pair<int, int> calc1(100, 200);
+	Pair<int, float> calc2(100, 200.0f);
+	Pair<int, double> calc3(100, 200.0f);
+	Pair<float, float> calc4(100.0f, 200.0f);
+	Pair<float, double> calc5(100.0f, 200.0f);
+	Pair<double, double> calc6(100.0f, 200.0f);
+
+	printf(" %d\n", calc1.Min<int>());
+	printf(" %f\n", calc2.Min<float>());
+	printf(" %lf\n", calc3.Min<double>());
+	printf(" %f\n", calc4.Min<float>());
+	printf(" %lf\n", calc5.Min<double>());
+	printf(" %lf\n", calc6.Min<double>());
 
 	return 0;
 }
