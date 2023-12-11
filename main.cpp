1bbd59a3d20970984f82eb20907abd860804f14a
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-12-11 11:49:26 +0900

    セーブ

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <list>
+#include <iostream>
+using namespace std;
 
 int main(void) {
-	printf("UTF-8になりました\n");
+	
+	list<const char*> yamamoto = { "1.Tokyo","2.kanda","3.Akihabara",""};
+
+	printf("1970year\n");
+	for (auto itr = yamamoto.begin(); itr != yamamoto.end(); ++itr) {
+		cout << *itr << "\n";
+	} 
+
 	return 0;
 }
