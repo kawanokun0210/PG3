880c40f7424e97b55fb0b6f0df832ec5a11e3f02
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2024-02-05 10:20:59 +0900

    終わり

--- main.cpp
+++ main.cpp
@@ -1,16 +1,58 @@
 #include <stdio.h>
-#include <list>
 #include <iostream>
+#include <windows.h>
+#include <list>
 using namespace std;
 
-int main(void) {
-	
-	list<const char*> yamanote = { "1.Tokyo","2.kanda","3.Akihabara",""};
+int main() {
+	list<const char*> YamanoteLine = {
+	"Tokyo","Kanda","Akihabara",
+	"Okachimachi","Ueno","Uguisudani",
+	"Nippori","Tabata","Komagome",
+	"Sugamo","Otsuka","Ikebukuro",
+	"Mejiro","Takadanobaba","Shin-Okubo",
+	"Shinjuku","Yoyogi","Harajuku",
+	"Shibuya","Ebisu","Meguro",
+	"Gotanda","Osaki","Shinagawa",
+	"Tamachi","Hamatsucho","Shimbashi","Yurakucho"
+	};
 
-	printf("1970year\n");
-	for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
-		cout << *itr << "\n";
-	} 
+	printf("1970\n");
+
+	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++) {
+		printf(*itr);
+		printf(",");
+	}
+
+	printf("\n");
+	printf("\n2019\n");
+
+	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++) {
+		if (*itr == "Tabata") {
+			itr = YamanoteLine.insert(itr, "Nishi-Nippori");
+			break;
+		}
+	}
+
+	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++) {
+		printf(*itr);
+		printf(",");
+	}
+
+	printf("\n");
+	printf("\n2022\n");
+
+	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++) {
+		if (*itr == "Tamachi") {
+			itr = YamanoteLine.insert(itr, "Takanawa-Gateway");
+			break;
+		}
+	}
+
+	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++) {
+		printf(*itr);
+		printf(",");
+	}
 
 	return 0;
 }
