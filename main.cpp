492e75cb00fc4a9c28df400f326b5cf7073f1686
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2024-02-12 11:54:00 +0900

    終わり

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <string>
-#include <chrono>
+﻿#include <stdio.h>﻿
+#include <iostream>
+#include <thread>
 
 int main() {
 
 	std::string a(100000, 'a');
 
-	
+	auto CountStart = std::chrono::system_clock::now();
+	std::string copy = a;
+	auto CountStop = std::chrono::system_clock::now();
+	std::chrono::duration<double, std::micro>Copy = CountStop - CountStart;
+
+	auto MoveStart = std::chrono::system_clock::now();
+	std::string move = std::move(a);
+	auto MoveStop = std::chrono::system_clock::now();
+	std::chrono::duration<double, std::micro>Move = MoveStop - MoveStart;
+
+	std::cout << Copy.count() << "μs" << std::endl;
+	std::cout << Move.count() << "μs" << std::endl;
 
 	return 0;
 }
